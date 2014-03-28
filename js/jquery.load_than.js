8c185cefb513e5e720622f6f09c3636b26cbf045
JBlond <Jblond@gmail.com>
2014-03-28 10:42:39 +0100

    load on scroll

---
+++ js/jquery.load_than.js
@@ -0,0 +1,13 @@
+(function(jQuery) {
+	jQuery.fn.loadimage = function(selector){
+		var $img = jQuery(this);
+		var data = $img.attr(selector);
+		$img.attr('src',data);
+	};
+	jQuery.fn.loadonscroll = function(){
+		jQuery(this).find('img').waypoint(function() {
+			jQuery(this).loadimage('data-src');
+		}, {offset:'100%', triggerOnce:true});
+		jQuery.waypoints('refresh');
+	};
+})(jQuery);
